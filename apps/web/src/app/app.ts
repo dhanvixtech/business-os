7f127b59cf9a4236b258d5ba0cb92491cfeadf7b
Ganeshk007 <ganesh100295@gmail.com>
2026-07-04 23:16:53 +0530

    feat(web): connect angular to health api

--- apps/web/src/app/app.ts
+++ apps/web/src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ApiService } from './core/services/api.service';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,29 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('web');
+
+  private api = inject(ApiService);
+
+  health: any;
+
+  ngOnInit() {
+
+    this.api.getHealth().subscribe({
+
+      next: (response) => {
+
+        console.log(response);
+
+        this.health = response;
+
+      },
+
+      error: (error) => {
+
+        console.error(error);
+
+      }
+
+    });
+  }
 }
